49e8a090fa136c465ad09413a21e0fd825558581
Futar <Futarimiti@163.com>
2023-10-21 23:43:18 +0100

    add interpretations for each action

--- lua/ide/const.lua
+++ lua/ide/const.lua
@@ -1,13 +1,13 @@
 local M = {}
 
-M.std_actions = { 'build'
-                , 'interpret'
-                , 'run'
-                , 'exec'
-                , 'debug'
-                , 'repl'
-                , 'repl_loaded'
-                , 'test'
+M.std_actions = { 'build'  -- build current file/project
+                , 'interpret'  -- interpret current file/project?
+                , 'run'  -- build & run current file/project
+                , 'exec'  -- without another build, run current file/project
+                , 'debug'  -- debug on the current file/project
+                , 'repl'  -- open a REPL for the current file/project
+                , 'repl_loaded'  -- open a REPL loading current file/project
+                , 'test'  -- run tests on current file/project
                 }
 
 M.std_modes = { 'single', 'proj' }
