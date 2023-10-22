b209eeb99fcfe6c25ad52011a38ada5b31f2be60
Futar <Futarimiti@163.com>
2023-10-22 18:39:24 +0100

    fmt

--- lua/ide/config/defaults.lua
+++ lua/ide/config/defaults.lua
@@ -1,42 +1,44 @@
 local typecheck = function (config)
-  vim.validate { setups = { config.setups, 'table' }
-               , ['setups.single'] = { config.setups.single, 'table' }
-               , ['setups.proj'] = { config.setups.proj, 'table' }
-               , quiet = { config.quiet, 'boolean' }
-               , command = { config.command, 'table' }
-               , ['command.enable'] = { config.command.enable, 'boolean' }
-               , ['command.name'] = { config.command.name, 'string' }
-               , ui = { config.ui, 'table' }
-               , ['ui.new'] = { config.ui.new, 'function' }
-               , ['ui.startinsert'] = { config.ui.startinsert, 'boolean' }
-               , ['ui.name'] = { config.ui.name, 'function' }
-               , write = { config.write, 'table' }
-               , other_actions = { config.other_actions, 'table' }
-               , other_modes = { config.other_modes, 'table' }
-               , filetype = { config.filetype, 'table' }
-               , ['filetype.detect'] = { config.filetype.detect, 'boolean' }
-               }
+  vim.validate
+  { setups = { config.setups, 'table' }
+  , ['setups.single'] = { config.setups.single, 'table' }
+  , ['setups.proj'] = { config.setups.proj, 'table' }
+  , quiet = { config.quiet, 'boolean' }
+  , command = { config.command, 'table' }
+  , ['command.enable'] = { config.command.enable, 'boolean' }
+  , ['command.name'] = { config.command.name, 'string' }
+  , ui = { config.ui, 'table' }
+  , ['ui.new'] = { config.ui.new, 'function' }
+  , ['ui.startinsert'] = { config.ui.startinsert, 'boolean' }
+  , ['ui.name'] = { config.ui.name, 'function' }
+  , write = { config.write, 'table' }
+  , other_actions = { config.other_actions, 'table' }
+  , other_modes = { config.other_modes, 'table' }
+  , filetype = { config.filetype, 'table' }
+  , ['filetype.detect'] = { config.filetype.detect, 'boolean' }
+  }
 end
 
-local defaults = { setups = { single = {}  -- core of the plugin - explained further in readme
-                            , proj = {}
-                            }
-                 , quiet = false  -- suppress log notifications?
-                 , command = { enable = true  -- enable the command?
-                             , name = 'IDE'  -- command name in one word; malformed command names may break things
-                             }
-                 , ui = { new = function ()  -- how would you like to bring up a new IDE terminal window?
-                                  vim.cmd.vsplit()  -- default: vsplit
-                                  return vim.api.nvim_get_current_win()  -- return window handle after you finish
-                                end
-                        , startinsert = true  -- enter insert mode after opening a new IDE terminal window?
-                        , name = function (_) return 'IDE term' end  -- how should I name the IDE terminal window, given the buf handle of the file you're editing?
-                        }
-                 , write = { 'interpret', 'debug', 'run', 'build', 'repl_loaded', 'test', 'typecheck' }  -- the buffer will be written before these actions are run
-                 , other_actions = {}  -- if you feel like standard actions are not enough, add your own here!
-                 , other_modes = {}  -- ditto, though single and proj should suffice really
-                 , filetype = { detect = true }  -- allow ide to detect filetype when it's not set
-                 }
+local defaults =
+{ setups = { single = {}  -- core of the plugin - explained further in readme
+           , proj = {}
+           }
+, quiet = false  -- suppress log notifications?
+, command = { enable = true  -- enable the command?
+            , name = 'IDE'  -- command name in one word; malformed command names may break things
+            }
+, ui = { new = function ()  -- how would you like to bring up a new IDE terminal window?
+                 vim.cmd.vsplit()  -- default: vsplit
+                 return vim.api.nvim_get_current_win()  -- return window handle after you finish
+               end
+       , startinsert = true  -- enter insert mode after opening a new IDE terminal window?
+       , name = function (_) return 'IDE term' end  -- how should I name the IDE terminal window, given the buf handle of the file you're editing?
+       }
+, write = { 'interpret', 'debug', 'run', 'build', 'repl_loaded', 'test', 'typecheck' }  -- the buffer will be written before these actions are run
+, other_actions = {}  -- if you feel like standard actions are not enough, add your own here!
+, other_modes = {}  -- ditto, though single and proj should suffice really
+, filetype = { detect = true }  -- allow ide to detect filetype when it's not set
+}
 
 assert(pcall(typecheck, defaults))
 
