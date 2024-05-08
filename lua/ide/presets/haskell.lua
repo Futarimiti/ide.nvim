12b0eb071a61c961c73937bedef77903d37de48b
Futar <Futarimiti@163.com>
2024-05-08 19:18:52 +0100

    stack: add interpret & metafunc to select resolver

--- lua/ide/presets/haskell.lua
+++ lua/ide/presets/haskell.lua
@@ -19,4 +19,26 @@ M.stack = { build = 'stack build'
           , test = 'stack test'
           }
 
+-- using a particular resolver
+---@param resolver string
+local stack = function (resolver)
+  if resolver == nil or resolver == '' then
+    return { build = 'stack build'
+           , run = 'stack run'
+           , repl_loaded = 'stack ghci'
+           , test = 'stack test'
+           , interpret = 'stack %s'
+           }
+  else
+    return { build = 'stack build --resolver ' .. resolver
+           , run = 'stack run --resolver ' .. resolver
+           , repl_loaded = 'stack ghci --resolver ' .. resolver
+           , test = 'stack test --resolver ' .. resolver
+           , interpret = 'stack script --resolver ' .. resolver .. ' %s'
+           }
+  end
+end
+
+setmetatable(M.stack, { __call = function (_, resolver) return stack(resolver) end })
+
 return M
